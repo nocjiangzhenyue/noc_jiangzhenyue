5105842e96f960202eb3dd9f3bbe8af68f8ff60e
jiangzhenyue <jiangzhenyue@chinatelecom.cn>
2021-08-30 15:30:47 +0800

    2021/08/30

---
+++ java-class1/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * 功能描述：
+ * 输出hello world
+ *
+ * @author jiangzhenyue
+ * @version 0.1.0
+ * @date 2021年08月30日 15:25
+ **/
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
